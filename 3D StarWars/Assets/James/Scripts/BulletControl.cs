6b1759daa6ff74a7f97b20449ae8689436396c2b
James Gentry <18251756+palpitatertot@users.noreply.github.com>
2018-01-24 08:55:59 -0600

    Bullets Spawn, trajectory is off

---
+++ 3D StarWars/Assets/James/Scripts/BulletControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour {
+	public float BulletSpeed = 12.0f;
+	private Vector3 _velocity;
+	// Use this for initialization
+	void Start () {
+		_velocity = BulletSpeed * transform.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawRay(transform.position + transform.forward, transform.forward, Color.red);
+		transform.Translate( _velocity * Time.deltaTime);
+	}
+}
--- 3D StarWars/Assets/James/Scripts/PlayerController.cs
+++ 3D StarWars/Assets/James/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour, IDamageable, IKillable {
-	private int LIGHT_SABER = 3, GROUND_CHECK = 2, CAMERA = 0;
+	private int LIGHT_SABER = 3, GROUND_CHECK = 2, CAMERA = 0, AIM_POINT = 5, BULLET_SPAWN = 4;
 	private enum SaberState {Idle, Blocking, Shooting};
 
 	public float PlayerRotSpeed = 500.0f;
@@ -11,8 +11,9 @@ public class PlayerController : MonoBehaviour, IDamageable, IKillable {
 	public float JumpHeight = 20.0f;
 	public float GroundDistance = .6f;
 	public float SaberSpeed = 5.0f;
-	public GameObject bulletPrefab;
-	public Transform bulletSpawn;
+	public GameObject BulletPrefab;
+	public Transform BulletSpawn;
+	public Transform AimPoint;
 
 	private CharacterController _controller;
 	private Vector3 _cameraOffset;
@@ -30,7 +31,7 @@ public class PlayerController : MonoBehaviour, IDamageable, IKillable {
 	// Use this for initialization
 	void Start () {
 		_saberState = SaberState.Idle;
-		_cameraOffset = new Vector3(transform.position.x, transform.position.y + 5.0f, transform.position.z + 8.0f);
+		_cameraOffset = new Vector3(transform.position.x, transform.position.y + 5.0f, transform.position.z - 8.0f);
 		_controller = GetComponent<CharacterController>();
 		_gravity = Physics.gravity.y;
 		_groundCheck = transform.GetChild(GROUND_CHECK);
@@ -44,25 +45,16 @@ public class PlayerController : MonoBehaviour, IDamageable, IKillable {
 	
 	// Update is called once per frame
 	void Update() {
-		//if(_saberState == SaberState.Shooting){
-		//	_saberAnimator.Play ("ShootSaber");
-		//	_saberState = SaberState.Idle;
-		//}
+		
 		_isGrounded = Physics.CheckSphere(_groundCheck.position, GroundDistance, _groundLayer, QueryTriggerInteraction.Ignore);
 		float deltaRotate = Input.GetAxis ("Mouse X") * PlayerRotSpeed;
 		transform.Rotate (0, deltaRotate, 0);
 		_cameraOffset = Quaternion.AngleAxis (Input.GetAxis ("Mouse X") * PlayerRotSpeed, Vector3.up) * _cameraOffset;
 		_camera.position = transform.position + _cameraOffset; 
 		_camera.LookAt(transform.position);
-		//var forward = _camera.forward;
-		//forward.y = 0;
-		//forward.Normalize ();
-		//transform.Rotate(0,Input.GetAxis ("Mouse X") * PlayerRotSpeed,0);
 			
 		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * PlayerMoveSpeed;
 
-		//move = move + transform.forward;
-		move = -move;
 		_velocity.y += _gravity * Time.deltaTime;
 
 		if (Input.GetKeyDown(KeyCode.Space) && _isGrounded) {
@@ -73,12 +65,13 @@ public class PlayerController : MonoBehaviour, IDamageable, IKillable {
 			_velocity.y = 0f;
 		}
 		move.y = _velocity.y;
-		//_controller.Move (move * Time.deltaTime);
 		transform.Translate(move * Time.deltaTime);
-
-
+		//Debug.DrawRay(transform.position + transform.forward, transform.forward, Color.green);
+		//Debug.DrawRay (BulletSpawn.transform.position + BulletSpawn.transform.forward, BulletSpawn.transform.forward, Color.green);
+		Transform aim = BulletSpawn.transform;
+		//aim.LookAt (AimPoint.transform);
+		Debug.DrawRay(aim.position + aim.forward, aim.forward, Color.red);
 		if(Input.GetMouseButtonDown(0) ){
-			//_saberState = SaberState.Shooting;	
 			_saberAnimator.Play ("ShootSaber");
 			StartCoroutine(Shoot ());
 		}
@@ -92,16 +85,12 @@ public class PlayerController : MonoBehaviour, IDamageable, IKillable {
 
 	private IEnumerator Shoot(){
 		yield return new WaitForSeconds(0.17f);
-		// Create the Bullet from the Bullet Prefab
+ 		//BulletSpawn.transform.LookAt(AimPoint.transform);
 		var bullet = (GameObject)Instantiate (
-			bulletPrefab,
-			bulletSpawn.position,
-			bulletSpawn.rotation);
+			BulletPrefab,
+			BulletSpawn.position,
+			BulletSpawn.rotation);
 
-		// Add velocity to the bullet
-		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 12;
-
-		// Destroy the bullet after 2 seconds
 		Destroy(bullet, 2.0f);
 	}
 		
