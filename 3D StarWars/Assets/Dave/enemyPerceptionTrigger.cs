32845ca9ed1998cb6ea6db36cd9d9138301100bf
Dave <david.faulk94@gmail.com>
2018-01-24 21:15:11 -0600

    David Faulk Working albiet simply enemy patroling and player pursuit.

---
+++ 3D StarWars/Assets/Dave/enemyPerceptionTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyPerceptionTrigger : MonoBehaviour {
+
+    public bool playerInArea;
+    public Transform pTrans;
+
+	// Use this for initialization
+	void Start () {
+        playerInArea = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerInArea = true;
+            pTrans = other.transform;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player" && playerInArea)
+        {
+            playerInArea = false;
+            pTrans = null;
+        }
+    }
+}
