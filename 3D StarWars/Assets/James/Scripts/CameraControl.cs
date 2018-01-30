4807e87e710078613d3277f98a5b123eb55302d9
James Gentry <18251756+palpitatertot@users.noreply.github.com>
2018-01-30 14:10:38 -0600

    camera Y axis first step implemented

--- 3D StarWars/Assets/James/Scripts/CameraControl.cs
+++ 3D StarWars/Assets/James/Scripts/CameraControl.cs
@@ -3,22 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
-	Vector3 _offset;
-	Transform _camera;
-	PlayerControl _player;
-	float rotSpeed;
+	private Vector3 _offset;
+	private Transform _camera;
+	private PlayerControl _player;
+	private float rotSpeed;
+	private float _cameraAngle;
+
 	public float yOffset = 2.0f;
 	public float zOffset = 0.0f;
 	public float minZOffset = -10f;
 	public float maxZOffset = -2f;
 
-	private float _rotSpeed;
+	private float _hRotSpeed;
+	public float VerticalRotationSpeed;
 
 	// Use this for initialization
 	void Start () {
 		_camera = transform.GetChild(0);
 		_player = GetComponent < PlayerControl> ();
-		_rotSpeed = _player.PlayerRotSpeed;
+		_hRotSpeed = _player.PlayerRotSpeed;
 		_offset = new Vector3(0, yOffset, zOffset);
 	}
 
@@ -28,6 +31,8 @@ public class CameraControl : MonoBehaviour {
 	}
 
 	void LateUpdate () {
+		_cameraAngle = Input.GetAxis ("Mouse Y");
+
 		if(Input.GetAxis("Mouse ScrollWheel") > 0){
 			zOffset += 1f;
 			Debug.Log ("Up" + zOffset);
@@ -48,7 +53,8 @@ public class CameraControl : MonoBehaviour {
 			}
 		}
 		//Vector3 cPos = _camera.localPosition;
-		_offset = Quaternion.AngleAxis (Input.GetAxis ("Mouse X") * _rotSpeed, Vector3.up) * _offset;
+		_offset= Quaternion.AngleAxis (Input.GetAxis ("Mouse X") * _hRotSpeed, Vector3.up) * _offset;
+		_offset = Quaternion.AngleAxis (Input.GetAxis ("Mouse Y") * VerticalRotationSpeed, transform.right) * _offset;
 		_camera.position = transform.position + _offset;
 		_camera.LookAt(transform.position);
 	}
