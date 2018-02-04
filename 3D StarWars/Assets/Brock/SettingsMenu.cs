0d1d2263674d12f4eeff779f21804f414c3624df
Brock Landry <brocklandry@Brocks-MacBook-Pro.local>
2018-02-04 16:18:14 -0600

    test commit for TMP

---
+++ 3D StarWars/Assets/Brock/SettingsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+	public void VolumeSlide (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
---
+++ 3D StarWars/Assets/Brock/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+	public void PlayButton ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
