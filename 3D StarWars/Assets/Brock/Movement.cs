594548884cc7cee1df3da0f5241152f28913b6fd
Brock Landry <brocklandry@Brocks-MacBook-Pro.local>
2018-02-05 14:07:51 -0600

    cage for leia

---
+++ 3D StarWars/Assets/Brock/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	void FixedUpdate () {
+		rb.AddForce (0, 0, 1000 * Time.deltaTime);
+
+		if (Input.GetKey ("d")) {
+			FindObjectOfType<PauseMenu> ().loseGame ();
+		}
+
+		if (Input.GetKey ("a")) {
+			FindObjectOfType<PauseMenu> ().winGame ();
+		}
+
+	}
+}
---
+++ 3D StarWars/Assets/Brock/Overseer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Overseer : MonoBehaviour {
+
+	public bool lukeDied = false;
+	public bool vaderDied = false;
+
+	public void loseGame ()
+	{
+		if (lukeDied == false) 
+		{
+			lukeDied = true;
+			SceneManager.LoadScene("MainMenu");
+		}
+
+	}
+
+
+	public void winGame ()
+	{
+		if (vaderDied == false) 
+		{
+			vaderDied = true;
+			SceneManager.LoadScene("Game");
+		}
+
+	}
+
+
+
+}
