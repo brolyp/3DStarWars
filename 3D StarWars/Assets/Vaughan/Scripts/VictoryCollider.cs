f93f2e89452c407705fdffe5640cdcbb8baa0aed
CollieSaloon <vcv5733@louisiana.edu>
2018-02-05 18:16:43 -0600

    touching Leia has begun

---
+++ 3D StarWars/Assets/Vaughan/Scripts/VictoryCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryCollider : MonoBehaviour {
+
+    public PauseMenu PauseMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" && Victory._deadVader)
+        {
+            WinGame();
+        }
+    }
+
+    private void WinGame()
+    {
+        PauseMenu.winGame();
+    }
+}
